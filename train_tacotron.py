74f9f225e8f15e7925433230fd403d1e551dcb7e
gyungsoo Kim <wlsl4239@naver.com>
2019-10-28 13:40:05 +0900

    39 start

--- train_tacotron.py
+++ train_tacotron.py
@@ -258,11 +258,11 @@ def main():
 
     parser.add_argument('--log_dir', default='logdir-tacotron')
     
-    parser.add_argument('--data_paths', default='./data/kss,./data/yuinna2')
+    parser.add_argument('--data_paths', default='./data/kss,./data/yuinna2,./data/39')
     
     
-    # parser.add_argument('--load_path', default=None)   # 아래의 'initialize_path'보다 우선 적용
-    parser.add_argument('--load_path', default='logdir-tacotron/kss+yuinna2_2019-10-23_15-47-37')
+    parser.add_argument('--load_path', default=None)   # 아래의 'initialize_path'보다 우선 적용
+    # parser.add_argument('--load_path', default='logdir-tacotron/kss+yuinna2_2019-10-23_15-47-37')
     
     parser.add_argument('--initialize_path', default=None)   # ckpt로 부터 model을 restore하지만, global step은 0에서 시작
 
